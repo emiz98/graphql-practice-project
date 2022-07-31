29ac961183a81cc58e838142288f5c1113194476
emiz98 <64089619+emiz98@users.noreply.github.com>
2022-07-31 07:33:10 +0530

    Connecting GraphQL using Apollo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import BookList from "./components/BookList";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
-});
+import { gql, useQuery } from "@apollo/client";
 
 function App() {
+  const GET_BOOKS = gql`
+    query {
+      books {
+        name
+        id
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
   return (
-    <ApolloProvider client={client}>
-      <div className="h-screen w-screen bg-red-900">
-        <BookList />
-      </div>
-    </ApolloProvider>
+    <div className="h-screen w-screen bg-red-900">
+      <BookList />
+    </div>
   );
 }
 
